3c323119800989433772009b67ffd12a65a2bc39
Muhammad Khezar <khezar_bangash2@hotmail.com>
2020-12-28 23:18:23 +0500

    testing with front end interface

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@
 
  console.log('Client side javascript file loaded!')
 
- fetch('http://localhost:3000/showtimes?hangu pakistan').then((response)=>{
+ /*fetch('http://localhost:3000/showtimes?address=hangu pakistan').then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             console.log(data.error)
@@ -14,4 +14,30 @@
             console.log(data)
         }
     })
- })
+ })*/
+ console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+
+
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    console.log(location)
+
+   /* fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })*/
+})
+
+ 
